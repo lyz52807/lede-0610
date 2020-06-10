f6c5eb82f1fd986f04f2430a05319f505381d4f5
lyz52807 <59039910+lyz52807@users.noreply.github.com>
2020-06-10 14:01:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,3 +10,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/vernesong/OpenClash package/openclash
+./scripts/feeds update -a
+./scripts/feeds install -a
